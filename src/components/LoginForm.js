992d819ceafc09f2727c26778e0b92db7dd6cb4f
Alex Yu <j620656786206@gmail.com>
2018-01-30 13:01:01 -0600

    116. More on Creating Immutable State

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged } from '../actions';
+import { emailChanged, passwordChanged } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 
 class LoginForm extends Component {
   onEmailChanged(text) {
     this.props.emailChanged(text);
+  }
+  onPasswordChanged(text) {
+    this.props.passwordChanged(text);
   }
 
   render() {
@@ -26,6 +29,8 @@ class LoginForm extends Component {
             secureTextEntry
             label="Password"
             placeholder="password"
+            onChangeText={this.onPasswordChanged.bind(this)}
+            value={this.props.password}
           />
         </CardSection>
 
@@ -41,8 +46,9 @@ class LoginForm extends Component {
 
 const mapStateToProps = state => {
   return {
-    email: state.auth.email
+    email: state.auth.email,
+    password: state.auth.password
   };
 };
 
-export default connect(mapStateToProps, { emailChanged })(LoginForm);
+export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
