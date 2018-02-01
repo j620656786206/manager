3bb702d783d8b55fbb08744e422dce1cd2f472eb
Alex Yu <j620656786206@gmail.com>
2018-01-31 18:15:15 -0600

    120. Redux Thunk in Practice Continued - bug: no console log shows up when press button

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import LoginForm from './components/LoginForm';
 class App extends Component {
 	componentWillMount() {
 		const config = {
-			apiKey: "AIzaSyDGMCwF-2vxh9_jFMi-b4APKZvWF5pi_TQ",
-			authDomain: "manager-7769e.firebaseapp.com",
-			databaseURL: "https://manager-7769e.firebaseio.com",
-			projectId: "manager-7769e",
-			storageBucket: "",
-			messagingSenderId: "221235630334"
+			apiKey: 'AIzaSyDGMCwF-2vxh9_jFMi-b4APKZvWF5pi_TQ',
+			authDomain: 'manager-7769e.firebaseapp.com',
+			databaseURL: 'https://manager-7769e.firebaseio.com',
+			projectId: 'manager-7769e',
+			storageBucket: '',
+			messagingSenderId: '221235630334'
 		};
 
 		firebase.initializeApp(config);
@@ -22,7 +22,7 @@ class App extends Component {
 
   render() {
 		const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
-		
+
 		return (
 			<Provider store={store}>
 				<LoginForm />
