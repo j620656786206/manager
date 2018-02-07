6892d9d105008eddf43b5f51effe5ffe8655f49c
Alex Yu <j620656786206@gmail.com>
2018-02-07 12:46:23 -0600

    134. Employee Form Actions

---
+++ src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,10 @@
+import EMPLOYEE_UPDATE from '../actions/types';
+
+const INITAL_STATE = {};
+
+export default (state = INITAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
