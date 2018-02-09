a3b297ab4ff067e3a21ff523849b343065cad3c2
Alex Yu <j620656786206@gmail.com>
2018-02-09 13:02:38 -0600

    139. Firebase JSPN Schema & 140. Data Security in Firebase & 141. Creating Records with Firebase

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -6,3 +6,7 @@ export const employeeUpdate = ({ props, value }) => {
     payload: { props, value }
   };
 };
+
+export const employeeCreate = ({ name, phone, shift }) => {
+  console.log(name, phone, shift);
+};
