e33b1fb79645b7c69934a905157065ca93d232da
Alex Yu <j620656786206@gmail.com>
2018-02-09 22:49:42 -0600

    145. Fetching Data from Firebase

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,6 +1,10 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types';
+import { 
+  EMPLOYEE_UPDATE, 
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
+ } from './types';
 
 export const employeeUpdate = ({ props, value }) => {
   return {
@@ -19,5 +23,16 @@ export const employeeCreate = ({ name, phone, shift }) => {
       dispatch({ type: EMPLOYEE_CREATE });
       Actions.pop();
     });
+  };
+};
+
+export const employeeFetch = () => {
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employee`)
+      .on('value', snapshot => {
+        dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
+      });
   };
 };
