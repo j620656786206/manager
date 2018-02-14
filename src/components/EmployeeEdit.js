9bd0733184aec3ff1eda768b93179287cc9148f0
Alex Yu <j620656786206@gmail.com>
2018-02-13 18:17:49 -0600

    154. Initializing Forms from State

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,15 +1,29 @@
-import React, { Compoenent } from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import EmployeeForm from './EmployeeForm';
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Button } from './common';
 
-class EmployeeEdit extends Compoenent {
+class EmployeeEdit extends Component {
+  componentWillMount() {
+    _.each(this.props.employee, (value, prop) => {
+      this.props.employeeUpdate({ prop, value });
+    });
+  }
+
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+    console.log(name, phone, shift);
+  }
+
+
   render() {
     return (
       <Card>
         <EmployeeForm />
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Save Changes
           </Button>
         </CardSection>
@@ -18,5 +32,10 @@ class EmployeeEdit extends Compoenent {
   }
 }
 
-export default connect()(EmployeeEdit);
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
 
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
