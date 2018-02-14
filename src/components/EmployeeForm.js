59f0213792901a1a344000709e3df8a355c379af
Alex Yu <j620656786206@gmail.com>
2018-02-14 14:27:47 -0600

    bug fixed- can change value employeeEdit form

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -62,4 +62,4 @@ const mapStateToProps = (state) => {
   return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, employeeUpdate)(EmployeeForm);
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeForm);
